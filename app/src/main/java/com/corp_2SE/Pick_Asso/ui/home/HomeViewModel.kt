0a4a4f92abc4524925b40873778a784162201ee1
LONGEFAY <mael.longefay-guinand@esme.fr>
2021-03-24 12:18:42 +0100

    1st version of Pick-Asso (link with Firebase)

---
+++ app/src/main/java/com/corp_2SE/Pick_Asso/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.corp_2SE.Pick_Asso.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
