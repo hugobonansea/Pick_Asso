045f33decaa9d264d437eb30c49312a63b2bf628
djiko <djiko753@gmail.Com>
2021-03-30 15:10:29 +0200

    Ajout d'un bouton avec action et espace de texte

--- app/src/main/java/com/corp_2SE/Pick_Asso/MainActivity.kt
+++ app/src/main/java/com/corp_2SE/Pick_Asso/MainActivity.kt
@@ -1,26 +1,38 @@
 package com.corp_2SE.Pick_Asso
 
+
+import android.app.Activity
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import com.google.firebase.ktx.Firebase
+
+
+data class User(val username: String, val email: String)
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+
+
+        val button = findViewById<Button>(R.id.sendData)
+        button.setOnClickListener {
+            val user = User("user_test", "email@test.fr")
+            Log.i("test" , user.username)
+            val editText = findViewById<EditText>(R.id.input_text)
+            val tap =editText.text
+            println("test")
+            println("try")
+            println(tap)
+        }
+
     }
 }
