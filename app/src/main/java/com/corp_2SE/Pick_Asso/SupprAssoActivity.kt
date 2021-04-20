dbfd37769437e0e1063760fd0225ee0e7def2ead
LONGEFAY <mael.longefay-guinand@esme.fr>
2021-04-20 19:10:55 +0200

    View XML seulement Suppr Asso (sous le principe des suppr de serv-discord à savoir demander de rentrer le nom complet de l'association pour supprimer l'association sinon ça ne marche pas)

---
+++ app/src/main/java/com/corp_2SE/Pick_Asso/SupprAssoActivity.kt
@@ -0,0 +1,11 @@
+package com.corp_2SE.Pick_Asso
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SupprAssoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_suppr_asso)
+    }
+}
