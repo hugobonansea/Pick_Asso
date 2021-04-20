47e8754e0cdddf6b7b35d54db669d338ff820d1b
LONGEFAY <mael.longefay-guinand@esme.fr>
2021-04-20 18:03:40 +0200

    View Modif Asso

---
+++ app/src/main/java/com/corp_2SE/Pick_Asso/UpdateAssoProfile.kt
@@ -0,0 +1,11 @@
+package com.corp_2SE.Pick_Asso
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UpdateAssoProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_asso_profile)
+    }
+}
