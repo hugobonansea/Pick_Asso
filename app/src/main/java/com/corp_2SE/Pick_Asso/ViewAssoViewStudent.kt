a2afd4223ed71a683bb9809edf9c6b3b3a5b484b
LONGEFAY <mael.longefay-guinand@esme.fr>
2021-04-28 11:29:39 +0200

    Plus de pages pour la partie ASSO (et intentiation des possibilités de modifier les informations d'une asso, de la suppr, bouton pour la partie écriture de msg ou création d'évents, etc...)

---
+++ app/src/main/java/com/corp_2SE/Pick_Asso/ViewAssoViewStudent.kt
@@ -0,0 +1,11 @@
+package com.corp_2SE.Pick_Asso
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewAssoViewStudent : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_asso_view_student)
+    }
+}
